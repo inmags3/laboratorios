ef623edfaf4e00b60b01a570e17df287ae16049d
inma.galvez <inmagalsan.plex@gmail.com>
2025-04-01 12:49:08 +0200

    subir data.txt y grupos.sh desde laboratorio anterior

---
+++ grupos.sh
@@ -0,0 +1,42 @@
+
+#!/bin/bash
+
+# Lista de géneros válidos
+valid_genres=("rock" "jazz" "metal" "alternative & punk" "rock and roll" "blues" "latin" "reggae" "pop" "soundtrack" "bossa nova" "easy listening" "heavy metal" "world" "classical")
+
+# Función para comprobar si un género está en la lista válida
+is_valid_genre() {
+	local genre_to_check="$1"
+	for valid in "${valid_genres[@]}"; do
+		if [[ "$genre_to_check" == "$valid"]]; then
+			return 0
+		fi
+	done
+
+# Leemos línea por línea del archivo data.txt
+
+while IFS=',' read -r title album genre; do
+	#Eliminamos espacios al principio y final
+	title=$(echo "$title" | sed 's/^ *//;s/ *$//')
+	album=$(echo "$album" | sed 's/^ *//;s/ *$//')
+	genre=$(echo "$genre" | sed 's/^ *//;s/ *$//')
+
+# Convertir "," por "|"
+
+line="$title|$album|$genre"
+
+# Usamos el nombre del género como nombre de archivo
+
+filename="$genre"
+
+# Comprobamos si el género es válido
+	if is_valid_genre"; then
+
+
+# Guardar la línea en el fichero correspondiente
+
+echo "$line" >> "$filename.txt"
+done < data.txt
+
+echo "Archivos generados:"
+ls *.txt grupos.sh
